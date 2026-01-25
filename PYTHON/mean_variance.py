d85173dc420d598eabac8e8e10fc34610147e5db
SJ <shivanshjha93@gmail.com>
2026-01-25 19:46:40 +0530

    yeah

---
+++ PYTHON/mean_variance.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    arr = np.array(list).reshape(3, 3)
+    return {
+        'mean': [arr.mean(axis=0).tolist(), arr.mean(axis=1).tolist(), arr.mean()],
+        'variance': [arr.var(axis=0).tolist(), arr.var(axis=1).tolist(), arr.var()],
+        'standard deviation': [arr.std(axis=0).tolist(), arr.std(axis=1).tolist(), arr.std()],
+        'max': [arr.max(axis=0).tolist(), arr.max(axis=1).tolist(), arr.max()],
+        'min': [arr.min(axis=0).tolist(), arr.min(axis=1).tolist(), arr.min()],
+        'sum': [arr.sum(axis=0).tolist(), arr.sum(axis=1).tolist(), arr.sum()]
+    }
